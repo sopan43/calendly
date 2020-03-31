11b91cea16bd271553616e6029aa0868da2908a1
Sopan <sopan.mittal@indiqube.com>
2020-03-31 21:58:54 +0530

    add config files

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const port = 3000;
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
+if (!process.env.NODE_ENV) {
+    process.env.NODE_ENV = 'dev';
+}
+
+const env = require('./config/' + process.env.NODE_ENV + '.json');
+let port = process.env.PORT || env.port;
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 
 app.listen(port, () => {
     console.log('Connection Established At PORT:', port);
