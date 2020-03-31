d25e3bc708ec5f4a5776b2a0ae5008221ac8443f
Sopan <sopan.mittal@indiqube.com>
2020-03-31 23:46:02 +0530

    FIX: add config in prod and staging

--- app.js
+++ app.js
@@ -14,16 +14,14 @@ app.use(bodyParser.urlencoded({
     extended: false
 }));
 
-console.log(process.env.NODE_ENV)
-
 //DB setup
 mongooose.set('useCreateIndex', true);
-// const conn = mongooose.connect(`mongodb://${process.env.CALENDLY_DB_USER}:${process.env.CALENDLY_DB_PASSWORD}@${env.db.url}:${env.db.port}/${env.db.databaseName}`, {useNewUrlParser: true, poolSize: 20}, (err) => {
-  //   if (err) {
-  //     console.log('Connection error. Mongo Service may be down');
-  //     process.exit();
-  //   }
-  // });
+const conn = mongooose.connect(`mongodb://${process.env.CALENDLY_DB_USER}:${process.env.CALENDLY_DB_PASSWORD}@${env.db.url}:${env.db.port}/${env.db.databaseName}`, {useNewUrlParser: true, poolSize: 20}, (err) => {
+    if (err) {
+      console.log('Connection error. Mongo Service may be down');
+      process.exit();
+    }
+  });
 
 app.listen(port, () => {
     console.log('Connection Established At PORT:', port);
